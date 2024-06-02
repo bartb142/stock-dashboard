db6a29b2392c7a0b3df565affd064ce7d2fe4ec7
bartb142 <bartb142@gmail.com>
2024-06-02 23:19:53 +0900

    Add Investment Records page.

---
+++ app/pages/2_Investment_Records.py
@@ -0,0 +1,57 @@
+import pandas as pd
+import streamlit as st
+import util
+
+util.common_page_config()
+
+"""
+# Investment Records
+"""
+
+THIS_YEAR = util.this_year()
+LAST_YEAR = util.last_year()
+INVESTMENT_RECORDS = util.INVESTMENT_RECORDS
+
+df = util.load_investment_record()
+annual_investment = util.load_annual_investment()
+this_year_investment = util.load_this_year_investment()
+last_year_investment = util.last_year_investment()
+with st.expander('Add new record'):
+    with st.form("new_invest_form"):
+        in_date = st.date_input('Date')
+        in_amount = st.number_input('Invest Amount (¥):',value=200000,format='%g',step=10000)
+        in_year = in_date.year
+        submitted = st.form_submit_button("Submit")
+        if submitted:
+            in_record = f'\n{in_date},{in_amount},{in_year}'
+            with open(INVESTMENT_RECORDS, 'at') as f:
+                f.write(in_record)
+            st.rerun()
+            st.write(in_record)
+            st.write('submitted!')
+col1,col2 = st.columns(2)
+with col1:
+    sec1,sec2 = st.columns(2)
+    with sec1:
+        st.metric(label=f'This Year ({THIS_YEAR})', value=f'¥{format(this_year_investment,',')}')
+    with sec2:
+        st.metric(label=f'Last Year ({LAST_YEAR})', value=f'¥{format(last_year_investment,',')}')
+    year_filter = st.multiselect(
+        label='Year filter:',
+        options=df['Year'].unique(),
+        default=THIS_YEAR
+    )
+    df_filtered = df.query('Year == @year_filter')[['Date','Amount']].sort_values(by='Date',ascending=False)
+    st.dataframe(df_filtered,
+                 hide_index=True,
+                 column_config={
+                    'Date': st.column_config.DateColumn(
+                        format='YYYY-MM-DD'
+                    ),
+                    'Amount': st.column_config.NumberColumn(
+                        'Amount(¥)',
+                    )
+                }
+    )
+with col2:
+    st.bar_chart(annual_investment, x='Year',y='Amount',height=550)
--- app/util.py
+++ app/util.py
@@ -3,6 +3,8 @@ from datetime import datetime
 import streamlit as st
 import pandas as pd
 
+STOCK_DIVIDEND = 'data/stock_dividend.csv'
+INVESTMENT_RECORDS = 'data/investment_records.csv'
 
 
 def this_year():
@@ -26,7 +28,7 @@ def common_page_config():
 
 # Dividend Dashboard
 def load_stock_dividend():
-    df = pd.read_csv('data/stock_dividend.csv', parse_dates=['入金日'])
+    df = pd.read_csv(STOCK_DIVIDEND, parse_dates=['入金日'])
     df['year'] = df['入金日'].dt.year
     df['dividend'] = df['受取金額[円/現地通貨]'].str.replace(',','').astype(int)
     return df
@@ -38,3 +40,17 @@ def get_total_dividend():
 def get_this_year_dividend():
     df = load_stock_dividend()
     return df[df['year'] == this_year()]['dividend'].sum()
+
+# Investment Records
+def load_investment_record():
+    return pd.read_csv(INVESTMENT_RECORDS,parse_dates=['Date'])
+
+def load_annual_investment():
+    df = load_investment_record()
+    return df[['Year','Amount']].groupby('Year',as_index=False).sum()
+def load_this_year_investment():
+    df = load_investment_record()
+    return df[df['Year'] == this_year()]['Amount'].sum()
+def last_year_investment():
+    df = load_investment_record()
+    return df[df['Year'] == last_year()]['Amount'].sum()
