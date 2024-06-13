beeed2b04da104e698334c25cdca2beadb0649b8
bartb142 <bartb142@gmail.com>
2024-06-13 23:05:09 +0900

    Add Stock Portfolio section

--- app/Home.py
+++ app/Home.py
@@ -20,3 +20,18 @@ col1,col2,col3,col4 = st.columns(4)
 with col1:
     if(total_dividend and this_year_dividend):
         st.metric(label="Total Dividend", value=f'¥{format(total_dividend,',')}', delta=f'¥{format(this_year_dividend,',')} ({THIS_YEAR}-now)')
+
+"""
+# Stock Portfolio
+"""
+if util.checkFile('data/stock_records.csv'):
+    df = pd.read_csv('data/stock_records.csv')
+    df['Total Stock Acquired'] = df['acquired_price'] * df['shares']
+    df = st.data_editor(df,
+                        hide_index=True,
+                        num_rows='dynamic',
+                        column_config={
+                            'stock_code': st.column_config.NumberColumn('Stock Code',format='%d')
+                        },
+                        disabled=['Total Stock Acquired'])
+    df.to_csv('data/stock_records.csv',index=False)
